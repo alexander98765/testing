abe791a887c1eebec07d2e6af41dfe62cec762d0
alexander98765 <alexandro3217@gmail.com>
2024-09-26 16:15:39 -0600

    upd yaml

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -67,6 +67,6 @@ def test_lambda_handler(apigw_event):
     ret = app.get_users(apigw_event, "")
     data = json.loads(ret["body"])
 
-    assert ret["statusCode"] == 202
+    assert ret["statusCode"] == 200
     #assert "message" in ret["body"]
     #assert data["message"] == "hello world"
